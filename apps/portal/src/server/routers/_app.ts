deae5989fa6eca1e251153aa1e6d36b37634c171
Dragoș Străinu <str.dr4605@gmail.com>
2026-06-02 10:07:38 +0300

    feat(api): register permissionDefinition router and invalidate sessions on permission change

--- apps/portal/src/server/routers/_app.ts
+++ apps/portal/src/server/routers/_app.ts
@@ -1,8 +1,10 @@
 import { router } from "../trpc";
 import { userRouter } from "./user";
+import { permissionDefinitionRouter } from "./permission";
 
 export const appRouter = router({
   user: userRouter,
+  permissionDefinition: permissionDefinitionRouter,
 });
 
 export type AppRouter = typeof appRouter;
